87a4e806409359e4ee8c55fb1a42d26520297863
Laurent FAURE <laurent.faure@2isa.org>
2016-11-17 16:45:05 +0100

    WEB SERVICES PART 3

--- WcfForadev/IServiceForadev.cs
+++ WcfForadev/IServiceForadev.cs
@@ -14,6 +14,19 @@ namespace WcfForadev
     [ServiceContract]
     public interface IServiceForadev
     {
+        #region CREATE
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+                   UriTemplate = "/AjoutReponse",
+                   BodyStyle = WebMessageBodyStyle.WrappedRequest,
+                   ResponseFormat = WebMessageFormat.Json)]
+        int? AjoutReponse(string idSujet, string idUtilisateur, string texteReponse, string dateReponse);
+
+        #endregion
+
+        #region RETRIEVE
+
         /// <summary>
         /// Renvoie la liste des rubriques du forum
         /// </summary>
@@ -49,7 +62,56 @@ namespace WcfForadev
         /// <returns></returns>
         [OperationContract]
         [WebGet(UriTemplate = "/Reponse/{titreSujet}",
-                ResponseFormat = WebMessageFormat.Xml)]
+                ResponseFormat = WebMessageFormat.Json)]
         List<Reponse> GetReponsesBySujet(string titreSujet);
+        #endregion
+
+        #region UPDATE
+
+        /// <summary>
+        /// Modifie un titre de sujet
+        /// </summary>
+        /// <param name="idSujet"></param>
+        /// <param name="TitreSujet"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebGet(UriTemplate = "/ModifierTitreSujet/{idSujet}/{titreSujet}",
+                ResponseFormat = WebMessageFormat.Json)]
+        int? ModifierTitreSujet(string idSujet, string TitreSujet);
+
+        /// <summary>
+        /// Modifie une description sujet
+        /// </summary>
+        /// <param name="idSujet"></param>
+        /// <param name="descSujet"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebGet(UriTemplate = "/ModifierDescriptionSujet/{idSujet}/{descSujet}",
+                ResponseFormat = WebMessageFormat.Json)]
+        int? ModifierDescriptionSujet(string idSujet, string descSujet);
+        #endregion
+
+        #region DELETE
+
+        /// <summary>
+        /// Supprime une réponse en fonction de son identifiant
+        /// </summary>
+        /// <param name="idReponse"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebGet(UriTemplate = "/SupprimerReponse/{idReponse}",
+                ResponseFormat = WebMessageFormat.Json)]
+        int? SupprimerReponse(string idReponse);
+
+        /// <summary>
+        /// Supprime un sujet et les réponses en cascade
+        /// </summary>
+        /// <param name="idReponse"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebGet(UriTemplate = "/SupprimerSujet/{idSujet}",
+                ResponseFormat = WebMessageFormat.Json)]
+        int? SupprimerSujet(string idSujet);
+        #endregion
     }
 }
--- WcfForadev/ServiceForadev.svc.cs
+++ WcfForadev/ServiceForadev.svc.cs
@@ -13,6 +13,23 @@ namespace WcfForadev
 {
     public class ServiceForadev : IServiceForadev
     {
+        #region CREATE
+
+        public int? AjoutReponse(string idSujet, string idUtilisateur, string texteReponse, string dateReponse)
+        {
+            int idSujetInt;
+            int idUtilisateurInt;
+            DateTime dateReponseDate;
+            if (int.TryParse(idSujet, out idSujetInt) & int.TryParse(idUtilisateur, out idUtilisateurInt) 
+                                                      & DateTime.TryParse(dateReponse, out dateReponseDate))
+                return DALMain.AjoutReponse(idSujetInt, idUtilisateurInt, texteReponse, dateReponseDate);
+            return null;
+        }
+
+        #endregion
+
+        #region RETRIEVE
+
         public List<Rubrique> GetRubriques()
         {
             return DALMain.GetRubriques();
@@ -31,6 +48,44 @@ namespace WcfForadev
         public List<Reponse> GetReponsesBySujet(string titreSujet)
         {
             return DALMain.GetReponsesBySujet(titreSujet);
+
         }
+
+        #endregion
+
+        #region UPDATE
+
+        public int? ModifierTitreSujet(string idSujet, string titreSujet)
+        {
+            int idSujetInt;
+            if (int.TryParse(idSujet, out idSujetInt)) return DALMain.ModifierTitreSujet(idSujetInt, titreSujet);
+            return null;
+        }
+
+      
+        public int? ModifierDescriptionSujet(string idSujet, string descSujet)
+        {
+            int idSujetInt;
+            if (int.TryParse(idSujet, out idSujetInt)) return DALMain.ModifierDescriptionSujet(idSujetInt, descSujet);
+            return null;
+        }
+        #endregion
+
+        #region DELETE
+
+        public int? SupprimerReponse(string idReponse)
+        {
+            int idReponseInt;
+            if (int.TryParse(idReponse, out idReponseInt)) return DALMain.SupprimerReponse(idReponseInt);
+            return null;
+        }
+
+        public int? SupprimerSujet(string idSujet)
+        {
+            int idSujetInt;
+            if (int.TryParse(idSujet, out idSujetInt)) return DALMain.SupprimerSujet(idSujetInt);
+            return null;
+        }
+        #endregion
     }
 }
